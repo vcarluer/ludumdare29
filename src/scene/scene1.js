e33f0967356c85de4420ec32f6b8e2d70983d2f0
Vincent Carluer <vcarluer@gmail.com>
2014-04-27 00:19:38 +0200

    landing

--- src/scene/scene1.js
+++ src/scene/scene1.js
@@ -28,14 +28,29 @@
 	};
 
 	Game.Scene.Scene1.prototype.prepare = function () {
+		var self = this;
 		this.game.currentScene = this;
-		this.shipScale = this.game.options.spaceShipHeight / this.game.options.pyxelCrop.height;
-		this.scaledTile = this.shipScale * this.game.options.tileSize;
+		this.shipScaleBase = this.game.options.spaceShipHeight / this.game.options.pyxelCrop.height;
+		this.shipScale = this.shipScaleBase;
+		this.getScaledTile = function () {
+			return self.shipScale * self.game.options.tileSize;
+		};
+
 		this.spaceship = {
-			x: (this.game.options.size - (this.game.options.pyxelCrop.width * this.shipScale)) / 2,
-			y: this.game.options.dialogHeight,
-			width: this.game.options.pyxelCrop.width * this.shipScale,
-			height: this.game.options.pyxelCrop.height * this.shipScale
+			xTarget: (this.game.options.size - (this.game.options.pyxelCrop.width * this.shipScale)) / 2,
+			yTarget: this.game.options.dialogHeight,
+			widthTarget: this.game.options.pyxelCrop.width * this.shipScale,
+			heightTarget: this.game.options.pyxelCrop.height * this.shipScale
+		};
+
+		this.spaceship.x = this.spaceship.xTarget;
+		this.spaceship.y = this.spaceship.yTarget;
+		this.spaceship.width = this.spaceship.widthTarget;
+		this.spaceship.height = this.spaceship.heightTarget;
+		this.spaceship.setScale = function (scale) {
+			self.shipScale = scale;
+			self.spaceship.width = self.game.options.pyxelCrop.width * self.shipScale;
+			self.spaceship.height = self.game.options.pyxelCrop.height * self.shipScale;
 		};
 
 		this.reactorRenderer = new Game.Renderer.SpriteRenderer({
@@ -46,23 +61,38 @@
 			loop: true
 		});
 
-		var yReactor = this.spaceship.y + this.spaceship.height;
 		this.reactors = [
 			{
-				x: this.spaceship.x + this.scaledTile,
-				y: yReactor
+				getX: function () {
+					return self.spaceship.x + self.getScaledTile();
+				},
+				getY: function () {
+					return self.spaceship.y + self.spaceship.height;
+				}
 			},
 			{
-				x: this.spaceship.x + 2 * this.scaledTile,
-				y: yReactor
+				getX: function () {
+					return self.spaceship.x + 2 * self.getScaledTile();
+				},
+				getY: function () {
+					return self.spaceship.y + self.spaceship.height;
+				}
 			},
 			{
-				x: this.spaceship.x + 4 * this.scaledTile,
-				y: yReactor
+				getX: function () {
+					return self.spaceship.x + 4 * self.getScaledTile();
+				},
+				getY: function () {
+					return self.spaceship.y + self.spaceship.height;
+				}
 			},
 			{
-				x: this.spaceship.x + 5 * this.scaledTile,
-				y: yReactor
+				getX: function () {
+					return self.spaceship.x + 5 * self.getScaledTile();
+				},
+				getY: function () {
+					return self.spaceship.y + self.spaceship.height;
+				}
 			}
 		];
 
@@ -89,17 +119,49 @@
 		};
 
 		this.planetDistance = 0;
-		this.planetGoal = 10;
+		this.planetGoal = 5;
+
+		this.landingTime = 0;
+		this.landingGoal = 1;
+		this.landingInfo = {
+			x: this.game.options.size / 2,
+			y: this.game.options.size / 4,
+			scale: 0
+		};
 	};
 
 	Game.Scene.Scene1.prototype.computePlanetCoord = function () {
 		var delta = this.planetDistance / this.planetGoal;
-		if (delta < 1) {
-			this.planet.x = this.planet.base.x + (this.planet.target.x - this.planet.base.x) * delta;
-			this.planet.y = this.planet.base.y + (this.planet.target.y - this.planet.base.y) * delta;
-			this.planet.width = this.planet.base.width + (this.planet.target.width - this.planet.base.width) * delta;
-			this.planet.height = this.planet.base.height + (this.planet.target.height - this.planet.base.height) * delta;
+		if (delta <= 1) {
+			this.planet.x = this.tween(delta, this.planet.base.x, this.planet.target.x);
+			this.planet.y = this.tween(delta, this.planet.base.y, this.planet.target.y);
+			this.planet.width = this.tween(delta, this.planet.base.width, this.planet.target.width);
+			this.planet.height = this.tween(delta, this.planet.base.height, this.planet.target.height);
+		} else {
+			this.planet.x = this.planet.target.x;
+			this.planet.y = this.planet.target.y;
+			this.planet.width = this.planet.target.width;
+			this.planet.height = this.planet.target.height;
+			this.planetReached = true;
 		}
+	};
+
+	Game.Scene.Scene1.prototype.computeSpaceshipLanding = function (deltaTime) {
+		this.landingTime += deltaTime / 1000;
+		var delta = this.landingTime / this.landingGoal;
+		if (delta <= 1) {
+			this.spaceship.x = this.tween(delta, this.spaceship.xTarget, this.landingInfo.x);
+			this.spaceship.y = this.tween(delta, this.spaceship.yTarget, this.landingInfo.y);
+			this.spaceship.setScale(this.tween(delta, this.shipScaleBase, this.landingInfo.scale));
+		} else {
+			this.spaceship.x = this.landingInfo.x;
+			this.spaceship.y = this.landingInfo.y;
+			this.spaceship.setScale(this.landingInfo.scale);
+		}
+	};
+
+	Game.Scene.Scene1.prototype.tween = function (delta, base, target) {
+		return base + (target - base) * delta;
 	};
 
 	Game.Scene.Scene1.prototype.update = function (delta) {
@@ -107,6 +169,9 @@
 
 		this.planetDistance += delta / 1000;
 		this.computePlanetCoord();
+		if (this.planetReached) {
+			this.computeSpaceshipLanding(delta);
+		}
 
 		this.game.ctx.clearRect(0, 0, this.game.dom.canvas.width, this.game.dom.canvas.height);
 
@@ -153,7 +218,9 @@
 		for (var reactorKey in this.reactors) {
 			if (this.reactors.hasOwnProperty(reactorKey)) {
 				reactor = this.reactors[reactorKey];
-				this.reactorRenderer.render(delta, reactor.x, reactor.y, this.scaledTile, this.scaledTile);
+				if (this.getScaledTile() > 0) {
+					this.reactorRenderer.render(delta, reactor.getX(), reactor.getY(), this.getScaledTile(), this.getScaledTile());
+				}
 			}
 		}
 
